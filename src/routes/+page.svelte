6d1f474d7dc1baa0e1228c50950168693134888d
Agate <me@agate.blue>
2024-08-16 22:31:46 +0200

    linting/format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,36 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import PouchDB from 'pouchdb'
+  import PouchDB from 'pouchdb';
 
-  import DiaryForm from '$lib/components/DiaryForm.svelte'
-  import DiaryFeedEntry from '$lib/components/DiaryFeedEntry.svelte'
-  
-  $: entries = []
+  import DiaryForm from '$lib/components/DiaryForm.svelte';
+  import DiaryFeedEntry from '$lib/components/DiaryFeedEntry.svelte';
+
+  $: entries = [];
 
   const db = new PouchDB('pesto');
 
   async function saveEntry(event: CustomEvent) {
-    let entry = event.detail as DiaryEntry
-    let savedEntry = await db.put(entry)
-    entry._rev = savedEntry.rev
-    entries = [entry, ...entries]
+    let entry = event.detail as DiaryEntry;
+    let savedEntry = await db.put(entry);
+    entry._rev = savedEntry.rev;
+    entries = [entry, ...entries];
   }
   onMount(async () => {
     let docs = await db.allDocs({
       include_docs: true,
       limit: 1000,
-      descending: true,
-    })
-    entries = docs.rows.map(d => {
-      return d.doc
-    })
-  })
+      descending: true
+    });
+    entries = docs.rows.map((d) => {
+      return d.doc;
+    });
+  });
 </script>
+
 <h1>Diary</h1>
 
 <DiaryForm on:created={saveEntry} cacheKey="new" />
 
 {#each entries as entry}
-  <DiaryFeedEntry bind:entry={entry} />
+  <DiaryFeedEntry bind:entry />
 {/each}
