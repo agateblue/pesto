68f746733f3078840944ef85119cfeb904529178
Agate <me@agate.blue>
2024-08-16 22:04:18 +0200

    Make pouchdb work

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	define: {global: "window"},   // <--- Add "window" here
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
