595bc48fc112727eccfd72cfdb081033ebbdb1df
Agate <me@agate.blue>
2024-08-17 17:37:16 +0200

    Added basic icons setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), Icons({compiler: 'svelte'})],
   define: { global: 'window' }, // <--- Add "window" here
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
