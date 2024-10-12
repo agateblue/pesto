308a23e670a977c6a0c05d1ca92af13ab74df84a
Agate <me@agate.blue>
2024-10-12 13:45:11 +0200

    Basic support for importing tempo tasks

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import MainNavigation from '$lib/components/MainNavigation.svelte';
-  import { buildUniqueId, type NoteDocument, globals } from '$lib/db';
+  import { buildUniqueId, type NoteDocType, globals } from '$lib/db';
 
   let files: File[];
 
@@ -22,38 +22,89 @@
     if (!entry.text) {
       return null
     }
-    let created_at = entry.date;
+    let created_at: string = entry.date;
     let msecs = Date.parse(created_at)
-    let id = buildUniqueId({
+    let id: string = buildUniqueId({
       msecs,
       random: Array(16).fill(0),
       seq: Number(0),
     })
-    let note: NoteDocument = {
-      id,
+    let content: string  = entry.text
+    let note: NoteDocType = {
+      id: id,
       title: null,
-      created_at: entry.date,
-      modified_at: entry.date,
+      created_at: created_at,
+      modified_at: created_at,
+      tags: [],
       fragments: {
         text: {
-          content: entry.text
+          content
         }
       }
     };
     return note;
   }
+
+  function getNoteFromTempoTask(task: object, doneList: number) {
+
+    // {"subtasks":[{"label":"Et rachat par Audrey","done":false},{"label":"Rachat de soulte","done":false},{"label":"Désolidarisation prêt","done":false}],"date":"2023-04-11T08:21:21.851Z","text":"Demander notaire pour succession maison","list":0,"category":null,"_id":"2023-04-11T08:21:21.851Z","_rev":"5-bd6f820ede5ef5fd8cff29c434d27962"}
+    if (!task.text) {
+      return null
+    }
+    let list: number = task.list
+    let created_at: string = task.date;
+    let msecs = Date.parse(created_at)
+    let id: string = buildUniqueId({
+      msecs,
+      random: Array(16).fill(0),
+      seq: Number(0),
+    })
+    let content: string  = task.text
+    let note: NoteDocType = {
+      id: id,
+      title: null,
+      created_at: created_at,
+      modified_at: created_at,
+      tags: [],
+      fragments: {
+        todolist: {
+          done: list >= doneList,
+          title: task.text,
+          todos: task.subtasks.map(t => {
+            return {
+              done: t.done,
+              text: t.label,
+            }
+          })
+        }
+      }
+    };
+    return note;
+  }
+
   async function handleImportTempo() {
     let tempoFile = files[0];
     let content = await tempoFile.text();
-    content = JSON.parse(content);
-    let entries: [] = content.entries;
-    console.log('Found', entries.length, 'entries to import…');
+    let data: object = JSON.parse(content);
+    let entries: [] = data.entries || [];
+    let tasks: [] = data?.board?.tasks || [];
+    let boardConfig: object = data?.board?.settings || {lists: []};
+    let doneList = boardConfig.lists.length
+
+    console.log('Found', entries.length + tasks.length, 'entries to import…');
 
     let notes: NoteDocument[] = [];
 
     for (const entry of entries) {
       let converted: NoteDocument | null = getNoteFromTempoEntry(entry);
-      console.log('CONVERTED', entry, converted);
+      console.log('CONVERTED Tempo note', entry, converted);
+      if (converted) {
+        notes.push(converted);
+      }
+    }
+    for (const task of tasks) {
+      let converted: NoteDocument | null = getNoteFromTempoTask(task, doneList);
+      console.log('CONVERTED Tempo task', task, converted);
       if (converted) {
         notes.push(converted);
       }
