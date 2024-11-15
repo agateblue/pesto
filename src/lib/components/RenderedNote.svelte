047fcb43e9ec22a7bc7100d5f80050ca39ab8eab
Agate <agate@pesto.garden>
2024-11-15 18:51:11 +0100

    Support for expand/collapse on long form content

--- src/lib/components/RenderedNote.svelte
+++ src/lib/components/RenderedNote.svelte
@@ -2,14 +2,16 @@
   import { type Database, type DocumentDocument, formatDate, getNoteUpdateData } from '$lib/db';
   import { renderMarkdown } from '$lib/ui';
   import TodoListFragmentEditor from './TodoListFragmentEditor.svelte';
+  import CollapsableContent from './CollapsableContent.svelte';
 
   interface Props {
     note: DocumentDocument;
     footer?: import('svelte').Snippet;
+    limitSize: boolean;
     [key: string]: any;
   }
 
-  let { note = $bindable(), footer, ...rest }: Props = $props();
+  let { note = $bindable(), footer, limitSize = true, ...rest }: Props = $props();
 </script>
 
 <article {...rest}>
@@ -17,9 +19,9 @@
     <time datetime={note.created_at}>{formatDate(note.created_at)}</time>
   </a>
   {#if note.fragments.text}
-    <div class="flow prose">
+    <CollapsableContent class="flow prose" {limitSize}>
       {@html renderMarkdown(note.fragments.text.content)}
-    </div>
+    </CollapsableContent>
   {/if}
   {#if note.fragments.todolist}
     <TodoListFragmentEditor
--- src/lib/ui.ts
+++ src/lib/ui.ts
@@ -85,3 +85,15 @@ export function updateURLParam(pageUrl, param: string, value: string) {
   query.set(param, value);
   return query;
 }
+
+export function getRandomId(length = 8) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
