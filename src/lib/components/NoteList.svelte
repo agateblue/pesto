a4470776bc714a005452f8bcbc0b7e22d5335e2e
Agate <me@agate.blue>
2024-11-15 16:39:49 +0100

    Moved note list into a dedicated component

---
+++ src/lib/components/NoteList.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+
+  import RenderedNote from '$lib/components/RenderedNote.svelte';
+  import { type DocumentDocument, globals, getQueryTokens, tokensToMangoQuery } from '$lib/db';
+
+
+  interface Props {
+    searchQuery: string;
+    orderQuery: string;
+  }
+
+  let { searchQuery = $bindable(), orderQuery = $bindable() }: Props = $props();
+
+  let notes: DocumentDocument[] = $state([]);
+
+  function getSortFromOrderQuery(o: string) {
+    let field, direction
+    [field, direction] = o.split(':')
+    let sort = {}
+    sort[field] = direction
+    return sort
+  }
+
+  function getSelector(q: string) {
+    if (!q.trim()) {
+      return {};
+    }
+
+    let tokens = getQueryTokens(q);
+    return { $and: tokensToMangoQuery(tokens) };
+  }
+
+  function loadNotes(q: string, o: string) {
+    return globals.db.documents.find({
+      limit: 20,
+      sort: [getSortFromOrderQuery(o)],
+      selector: {type: 'note', ...getSelector(q)}
+    }).$.subscribe(documents => {
+      notes = documents
+    });
+  }
+
+  let subscription = null
+  $effect(() => {
+    subscription?.unsubscribe();
+    subscription = loadNotes(searchQuery, orderQuery);
+  });
+</script>
+
+<div class="wrapper | m__block-3" role="list">
+  {#each notes as note}
+    {#key note._rev}
+      <RenderedNote {note} class="diary__note flow | p__block-3" role="listitem"></RenderedNote>
+    {/key}
+  {/each}
+  {#if notes.length > 0}
+    <button
+      onclick={async (e) => {
+        let newNotes = await globals.db.documents.find({
+          limit: 20,
+          sort: [getSortFromOrderQuery(orderQuery)],
+          selector: {
+            type: 'note',
+            id: { $lt: notes.slice(-1)[0].id },
+            ...getSelector(searchQuery)
+          }
+        }).exec();
+        notes = [...notes, ...newNotes];
+      }}>Load more</button
+    >
+  {/if}
+</div>
