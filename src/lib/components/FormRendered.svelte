4f73425acd1dadf62d9fb116a855700243af51df
Agate <agate@pesto.garden>
2024-12-29 02:08:26 +0100

    Added reuse last values form

--- src/lib/components/FormRendered.svelte
+++ src/lib/components/FormRendered.svelte
@@ -74,8 +74,18 @@
       <FormFieldRendered {field} bind:value={v[field.id]} />
     {/each}
 
-    <button type="submit">Save…</button>
-
+    <div class="flex__row flex__justify-between">
+      <button type="submit">Save</button>
+      <button type="button" class="button__link" onclick={async () => {
+        let last = await globals.db.documents.findOne({
+          selector: {'fragments.form.id': form.id},
+          sort: [{id: 'desc'}]
+        }).exec()
+        if (last) {
+          v = cloneDeep(last.fragments.form.data)
+        }
+      }}>Use last values</button>
+    </div>
     {@render children?.()}
   </form>
 </div>
