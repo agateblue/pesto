c979aaff8e1b02abfcf55e69d07eabe28e09e776
Agate <me@agate.blue>
2024-12-31 06:43:25 +0100

    Added plausible integration

---
+++ src/lib/plausible.ts
@@ -0,0 +1,17 @@
+import Plausible from 'plausible-tracker'
+import { PUBLIC_PLAUSIBLE_HOST, PUBLIC_PLAUSIBLE_DOMAIN} from '$env/static/public';
+
+import { navigating } from '$app/stores';
+
+const { trackPageview } = Plausible({
+  domain: PUBLIC_PLAUSIBLE_DOMAIN,
+  // Track localhost by default
+  trackLocalhost: !!PUBLIC_PLAUSIBLE_HOST,
+  apiHost: PUBLIC_PLAUSIBLE_HOST
+})
+
+export function trackRouteChange (page) {
+  trackPageview({
+    url: page.route.id,
+  })
+}
