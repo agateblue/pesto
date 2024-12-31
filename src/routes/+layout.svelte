8108729c78b08d88c7f4d3f1844be9d9dc2f4ab6
Agate <me@agate.blue>
2024-12-31 07:21:51 +0100

    Fixed service worker crash on firefox on localhost

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
   import { afterNavigate } from '$app/navigation';
   import { trackRouteChange } from '$lib/plausible';
   import { page } from '$app/stores';
+  import { dev } from '$app/environment';
   import { globals } from '$lib/db';
+  import { onMount } from 'svelte';
   /**
    * @typedef {Object} Props
    * @property {import('svelte').Snippet} [children]
@@ -14,6 +16,20 @@
   afterNavigate(() => {
     globals.uiState.set('currentPage', () => null);
     trackRouteChange($page)
+  });
+
+  // to avoid firefox crashing locally because service worker can't register
+  onMount(async () => {
+    if (dev) {
+      console.warn('Will not register service worker in dev mode.');
+    } else {
+      if ('serviceWorker' in navigator) {
+        console.info('Registering service worker…');
+        navigator.serviceWorker.register('/service-worker.js', {
+          type: dev ? 'module' : 'classic',
+        });
+      }
+    }
   });
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
+    serviceWorker: {
+      register: false,
+    },
     adapter: adapter({
       // default options are shown. On some platforms
       // these options are set automatically — see below
