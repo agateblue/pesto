9eb1cf6dd5e16b7ac06c0122e2e1ef7cb877a59d
Agate <agate@pesto.garden>
2024-12-31 08:40:08 +0100

    Support for sharing to note

--- src/routes/my/notes/add/+page.svelte
+++ src/routes/my/notes/add/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import NoteForm from '$lib/components/NoteForm.svelte';
-  import { goto } from '$app/navigation';
+  
+  let { data } = $props();
+
 </script>
 
 <main class="wrapper | flex__grow">
-  <NoteForm note={null}>
+  <NoteForm note={data.note || null}>
     <a href="/my" class="button"> Save </a>
   </NoteForm>
 </main>
---
+++ src/routes/share-receiver/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import LoadingState from '$lib/components/LoadingState.svelte';
+  import MainNavigation from '$lib/components/MainNavigation.svelte';
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { page } from '$app/stores';
+  import { globals } from '$lib/db';
+  import { delay } from '$lib/ui';
+  
+  let isLoading = $state(true)
+  onMount(async () => {
+    let title = $page.url.searchParams.get("title")
+    let text = $page.url.searchParams.get("text")
+    let url = $page.url.searchParams.get("url")
+  
+    if (!title && !text && !url) {
+      console.warn("Empty share")
+      await goto('/')
+    }
+    let noteParts = []
+    if (title) {
+      noteParts.push(`# ${title}`)
+    }
+  
+    if (url) {
+      noteParts.push(url)
+    }
+  
+    if (text) {
+      noteParts.push(text)
+    }
+  
+    let body = noteParts.join("\n\n")
+    
+    await globals.uiState.set('sharedNote', () => body);
+    await delay(500)
+    await goto('/my/notes/add?from=share')
+  })
+</script>
+
+<div class="my__layout">
+  <MainNavigation />
+  <main class="p__block-4">
+    <LoadingState {isLoading}>Processing share…</LoadingState>
+  </main>
+</div>
