ac43eaecbf70b1fd9209adae6b712d423f97075a
Agate <me@agate.blue>
2025-01-01 02:15:26 +0100

    Include release ID for sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,11 +1,12 @@
-import { PUBLIC_SENTRY_DSN } from '$env/static/public';
+import { PUBLIC_SENTRY_DSN, PUBLIC_BUILD_ID } from '$env/static/public';
 
 import { handleErrorWithSentry } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
   dsn: PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0
+  tracesSampleRate: 1.0,
+  release: PUBLIC_BUILD_ID,
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
