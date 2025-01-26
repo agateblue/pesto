73a8b27f17fb78052068c8a3a5fb316252625bd7
Agate <agate@pesto.garden>
2025-01-26 20:32:22 +0100

    Fixed 500 on page refresh

--- src/routes/my/+page.ts
+++ src/routes/my/+page.ts
@@ -4,7 +4,7 @@ export async function load({url}) {
   let collection = null
   let collectionId = url.searchParams.get('collection')
   if (collectionId) {
-    collection = (await globals.db?.documents.findOne({
+    collection = (await globals.db?.documents?.findOne({
       selector: {id: collectionId, type: 'collection'}
     }).exec())?.toMutableJSON()
   }
