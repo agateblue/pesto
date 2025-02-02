46033e679b6dda3f1e25fb82174a5d512c97eeb8
Agate <agate@pesto.garden>
2025-02-02 12:46:59 +0100

    Basic setup to extract/compile translations

---
+++ src/i18n/extract.cjs
@@ -0,0 +1,35 @@
+const { GettextExtractor, RegexExtractors  } = require('@rgglez/gettext-extractor');
+const pofile = require("pofile");
+const fs = require("fs");
+
+let extractor = new GettextExtractor();
+
+extractor
+    .createRegexParser([
+        RegexExtractors.addCondition({
+            regex: /\$_\(['"`](.*?)['"`]\)/i,
+            text: 1
+        })
+    ])
+    .parseFilesGlob('./src/**/*.@(ts|js|tsx|jsx|svelte)');
+
+extractor
+    .createRegexParser([
+        RegexExtractors.addCondition({
+            regex: /\$_n\(['"`](.*?)['"`]\)/i,
+            text: 1,
+            textPlural: 2
+        })
+    ])
+    .parseFilesGlob('./src/**/*.@(ts|js|tsx|jsx|svelte)');    
+
+function getPotString(headers = {}) {
+    const po = new pofile();
+    po.items = extractor.getPofileItems().sort((a, b) => (a.references.sort()[0] > b.references.sort()[0]) ? 1 : -1);
+    po.headers = Object.assign({'Content-Type': 'text/plain; charset=UTF-8'}, headers);
+    return po.toString();
+}
+
+fs.writeFileSync('./src/i18n/messages/messages.pot', getPotString());
+
+extractor.printStats();
