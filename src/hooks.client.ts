ec7ada27a16b227e8c00749f72dbb2baac647034
Agate <1970915+agateblue@users.noreply.github.com>
2025-02-11 00:06:33 +0100

    Disabled sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,13 +1,13 @@
-import { PUBLIC_SENTRY_DSN, PUBLIC_BUILD_ID } from '$env/static/public';
+// import { PUBLIC_SENTRY_DSN, PUBLIC_BUILD_ID } from '$env/static/public';
 
-import { handleErrorWithSentry } from '@sentry/sveltekit';
-import * as Sentry from '@sentry/sveltekit';
+// import { handleErrorWithSentry } from '@sentry/sveltekit';
+// import * as Sentry from '@sentry/sveltekit';
 
-Sentry.init({
-  dsn: PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  release: PUBLIC_BUILD_ID
-});
+// Sentry.init({
+//   dsn: PUBLIC_SENTRY_DSN,
+//   tracesSampleRate: 1.0,
+//   release: PUBLIC_BUILD_ID
+// });
 
-// If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+// // If you have a custom error handler, pass it to `handleErrorWithSentry`
+// export const handleError = handleErrorWithSentry();
