cdc2b2af2950070cf04f52c9fdb4726986991dc2
Agate <1970915+agateblue@users.noreply.github.com>
2025-02-11 00:13:32 +0100

    Lazy load emoji picker

--- src/lib/components/EmojiPicker.svelte
+++ src/lib/components/EmojiPicker.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-  import { Picker } from 'emoji-picker-element';
   import { onDestroy, onMount } from 'svelte';
   interface Props {
     onEmojiClick: Function;
   }
   let { onEmojiClick }: Props = $props();
-
+  
   let el
   // we singleton the emoji picker to avoid 
   // https://github.com/nolanlawson/emoji-picker-element/issues/329
-  onMount(() => {
+  onMount(async () => {
+    const p = await import('emoji-picker-element');
     if (!window._emojipicker) {
-      window._emojipicker = new Picker();
+      window._emojipicker = new p.Picker();
       window._emojipicker.dataSource = '/emojis.json';
     } 
     el.appendChild(window._emojipicker);
