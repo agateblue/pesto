024d62cba7b7bdef866357324e42480a7f1a4226
Agate <1970915+agateblue@users.noreply.github.com>
2025-02-12 00:25:36 +0100

    Enforce CSP in built files

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const CONNECT_SRC = (process.env.VITE_CSP_ALLOWED_CONNECT_SRC || '').split(',').filter(e => {
+  return e.trim()
+}).map(e => {
+  return e.trim()
+})
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -19,7 +25,17 @@ const config = {
       fallback: '404.html',
       precompress: false,
       strict: true
-    })
+    }),
+    csp: {
+			directives: {
+        'default-src': ['self'],
+        'script-src': ['self', 'unsafe-eval'],
+        'style-src': ['self'],
+        'style-src-attr': ['self', 'unsafe-inline'],
+        'style-src-elem': ['self', 'unsafe-inline'],
+        'connect-src': ['self', ...CONNECT_SRC],
+			},
+		}
   }
 };
 
